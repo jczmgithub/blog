1abf46e11ffd1ce5cb4db548f1cfda1b904a9141
“Juan” <jcasinoste16dw@ikzubirimanteo.com>
2017-09-29 13:41:48 +0200

    u2t6 BBDD

--- app/modeloDatos.php
+++ app/modeloDatos.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class modeloDatos extends Model
 {
-    //
+    protected $fillable = [
+        'datos',
+    ];
+
+    protected $hidden = [
+        'remember_token',
+    ];
 }
