677584452ef8afd90387beb31f105e4bac40cf2c
“Juan” <jcasinoste16dw@ikzubirimanteo.com>
2017-09-29 13:36:56 +0200

    u2t6 BBDD

--- resources/views/datosBBDD.blade.php
+++ resources/views/datosBBDD.blade.php
@@ -8,7 +8,7 @@
     </head>
     <body>
 
-	Los datos enviados a la BBDD son <?php echo $_POST["datosBBDD"]; ?>
+	Los datos enviados a la BBDD son: <?php echo $_POST["datosBBDD"]; ?>
 
     </body>
 </html>
